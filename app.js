c50b681edf74d6763415d13e6fa20e32e5009fe9
ZSabakh <zurabsabakhtarishvili@gmail.com>
2020-11-13 18:27:20 -0600

    SQL introduction

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path')
-const notFound = require('./controllers/404')
+const path = require("path");
+const notFound = require("./controllers/404");
+const sql = require("./utility/sql");
+
 const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views','views')
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+sql.execute("SELECT * FROM items").then().catch();
 
 app.use(bodyParser.urlencoded());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
